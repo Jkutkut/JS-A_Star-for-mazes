f78286835e2b56fe39651e05fa344eb763e70544
jkutkut <jorgeregonzalez@gmail.com>
2021-06-03 15:50:15 +0200

    code adapted to the controls

--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
 var maze;
+var currentMazeType;
 var w = 13;
 var h = w;
 
-var onLoop = true;
+var onLoop;
 var aStarIterator;
 
-function setup() {
+function preload() {
     if (confirm('Would you like a maze?')) {
-        maze = new Maze(windowWidth, windowHeight, w, h);
+        currentMazeType = Maze;
     } else {
-        maze = new RandomMaze(windowWidth, windowHeight, w, h);
+        currentMazeType = RandomMaze;
     }
+}
+
+function setup() {
+    maze = new currentMazeType(windowWidth, windowHeight, w, h);
+
+    onLoop = true
 
     rows = Math.floor(windowWidth / w);
     if (rows % 2 == 0) rows--;
@@ -34,7 +41,6 @@ function draw() {
         maze.path[i].show(Maze.COLORS.cGrey);
     }
 
-    // maze.nextA_StarStep();
     aStarIterator.next();
 
     for (var i = 0; i < maze.path.length; i++) {
@@ -44,23 +50,46 @@ function draw() {
 }
 
 function keyPressed(event) {
-    console.log(event);
-
     switch(event.key) {
-        case " ":
+        case " ": // Stop/Resume the current execution
             if (onLoop) noLoop();
             else loop();
             onLoop = !onLoop;
             break;
-        case "ArrowRight":
+        case "ArrowRight": // Execute a sigle step
             draw();
             break;
-        case "f":
+        case "f": // Fast-foward to the end
             while (!aStarIterator.next().done){}
 
             for (let p of maze.closedSet) {
                 p.show(Maze.COLORS.cGrey);
             }
             draw();
+            break;
+        case "s": // Change the size of the cells of the grid
+            let sol = window.prompt("New size of each cell?", w);
+            if (sol != null &&  // Check sol is a valid number. Source: stackoverflow.com/questions/175739
+                !isNaN(sol) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+                !isNaN(parseFloat(sol))) { // ...and ensure strings of whitespace fail)
+                
+                console.log("Changing the size of the cell to " + sol + "x" + sol + "px");
+                w = sol;
+                h = w;
+            }
+            else {
+                console.warn("Value entered is not a valid number");
+                break;
+            }
+            // Also do a reset
+        case "r": // Reset with the current configuration
+            setup();
+            loop();
+            break;
+        case "t":
+            preload();
+            setup();
+            loop();
+            break;
     }
 }
