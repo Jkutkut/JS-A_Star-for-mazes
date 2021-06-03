56737365de27d91073a9b286755ea19f1884a920
jkutkut <jorgeregonzalez@gmail.com>
2021-06-03 15:10:06 +0200

    logic with new aStar method updated. Keys controls added to stop/resume the algo, to move just one step and fast-foward to the end

--- sketch.js
+++ sketch.js
@@ -2,9 +2,15 @@ var maze;
 var w = 13;
 var h = w;
 
+var onLoop = true;
+var aStarIterator;
+
 function setup() {
-    // maze = new RandomMaze(windowWidth, windowHeight, w, h);
-    maze = new Maze(windowWidth, windowHeight, w, h);
+    if (confirm('Would you like a maze?')) {
+        maze = new Maze(windowWidth, windowHeight, w, h);
+    } else {
+        maze = new RandomMaze(windowWidth, windowHeight, w, h);
+    }
 
     rows = Math.floor(windowWidth / w);
     if (rows % 2 == 0) rows--;
@@ -12,6 +18,8 @@ function setup() {
     if (cols % 2 == 0) cols--;
 
     createCanvas(rows * w, cols * h);
+
+    aStarIterator = maze.aStar();
 
     strokeWeight(0);
     for (let i = 0; i < maze.rows; i++) {
@@ -26,10 +34,33 @@ function draw() {
         maze.path[i].show(Maze.COLORS.cGrey);
     }
 
-    maze.nextA_StarStep();
+    // maze.nextA_StarStep();
+    aStarIterator.next();
 
     for (var i = 0; i < maze.path.length; i++) {
         maze.path[i].show(Maze.COLORS.cBlue);
     }
     maze.end.show(Maze.COLORS.cRed);
+}
+
+function keyPressed(event) {
+    console.log(event);
+
+    switch(event.key) {
+        case " ":
+            if (onLoop) noLoop();
+            else loop();
+            onLoop = !onLoop;
+            break;
+        case "ArrowRight":
+            draw();
+            break;
+        case "f":
+            while (!aStarIterator.next().done){}
+
+            for (let p of maze.closedSet) {
+                p.show(Maze.COLORS.cGrey);
+            }
+            draw();
+    }
 }
