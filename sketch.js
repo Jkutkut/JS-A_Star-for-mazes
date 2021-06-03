a50dc6ca4d692dd79a0263784b8f180ab589eef9
jkutkut <jorgeregonzalez@gmail.com>
2021-06-03 13:30:06 +0200

    spacing updated

--- sketch.js
+++ sketch.js
@@ -1,36 +1,35 @@
 var maze;
-var w = 10;
+var w = 13;
 var h = w;
 
 function setup() {
-  // maze = new RandomMaze(windowWidth, windowHeight, w, h);
-  maze = new Maze(windowWidth, windowHeight, w, h);
-  
-  rows = Math.floor(windowWidth / w);
-  if (rows % 2 == 0) rows--;
-  cols = Math.floor(windowHeight / h);
-  if (cols % 2 == 0) cols--;
+    // maze = new RandomMaze(windowWidth, windowHeight, w, h);
+    maze = new Maze(windowWidth, windowHeight, w, h);
 
-  createCanvas(rows * w, cols * h);
+    rows = Math.floor(windowWidth / w);
+    if (rows % 2 == 0) rows--;
+    cols = Math.floor(windowHeight / h);
+    if (cols % 2 == 0) cols--;
 
-  strokeWeight(0);
-  for (let i = 0; i < maze.rows; i++) {
-      for (let j = 0; j < maze.cols; j++) {
-          maze.grid[i][j].show();
-      }
-  }
+    createCanvas(rows * w, cols * h);
 
+    strokeWeight(0);
+    for (let i = 0; i < maze.rows; i++) {
+        for (let j = 0; j < maze.cols; j++) {
+            maze.grid[i][j].show();
+        }
+    }
 }
 
 function draw() {
-  for (var i = 0; i < maze.path.length; i++) {
-    maze.path[i].show(Maze.COLORS.cGrey);
-  }
+    for (var i = 0; i < maze.path.length; i++) {
+        maze.path[i].show(Maze.COLORS.cGrey);
+    }
 
-  maze.nextA_StarStep();
-  
-  for (var i = 0; i < maze.path.length; i++) {
-    maze.path[i].show(Maze.COLORS.cBlue);
-  }
-  maze.end.show(Maze.COLORS.cRed);
+    maze.nextA_StarStep();
+
+    for (var i = 0; i < maze.path.length; i++) {
+        maze.path[i].show(Maze.COLORS.cBlue);
+    }
+    maze.end.show(Maze.COLORS.cRed);
 }
